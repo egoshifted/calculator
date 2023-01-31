8494a10386d0e4810d812a08939de1f5ee71e7a8
Anton <adjatsuk@protonmail.com>
2023-01-31 23:14:25 +0000

    Added event listeners and classes/ids to buttons

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 const keys = document.getElementById('keys');
-const keySelect = document.querySelector(`[id="keys"]`)
-const resultValue = document.getElementById('result-value').innerText
+let resultValue = document.getElementById('result-value').innerText
 
-//Set ID to keys on window load
-window.onload = function() {
-  for(i = 0; i < keys.children.length; i++) {
-    const currentKeyName = keys.children[i].innerText;
-    keys.children[i].id = currentKeyName;
-  }
+// Set ID to keys on window load (Operators & Numbers)
+window.onload = function () {
+    for (i = 0; i < keys.children.length; i ++) {
+        const currentKeyName = keys.children[i].innerText;
+        keys.children[i].id = currentKeyName;
+        keys.children[i].addEventListener('click', testFunc)
+        switch (currentKeyName) {
+            case "C": keys.children[i].className = "operator";
+                break;
+            case "+/-": keys.children[i].className = "operator";
+                break;
+            case "/": keys.children[i].className = "operator";
+                break;
+            case "%": keys.children[i].className = "operator";
+                break;
+            case "*": keys.children[i].className = "operator";
+                break;
+            case "-": keys.children[i].className = "operator";
+                break;
+            case "+": keys.children[i].className = "operator";
+                break;
+            case ".": keys.children[i].className = "point";
+                break;
+            case "←": keys.children[i].className = "backspace";
+                break;
+            case "=": keys.children[i].className = "equals";
+                break;
+            default: keys.children[i].className = "number";
+
+        }
+    }
 }
 
+function testFunc(e) {
+  console.log(` ${e.target.id} clicked!`)
+}
